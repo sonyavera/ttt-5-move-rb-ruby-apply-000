f8eb46ccd1773ac35fe340c12994d52a1bbefe7c
Sonya Gould <sonyavgould@gmail.com>
2020-05-20 17:52:00 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,13 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index(input)
+  input_as_an_integer = input.to_i
+  input_as_an_integer - 1 #should array index should be input - 1?
+end
+
+def move(array, index, character = "X")
+  array[index]= character
+  array
+end
